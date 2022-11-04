07edecc7437815513bd7fcb4c9719a0e1599a083
Huydt13 <huydt@bakco.com.vn>
2022-11-04 11:29:17 +0700

    chore : config sanity

---
+++ sanity/schemas/social.js
@@ -0,0 +1,18 @@
+export default {
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      description: "Platfrom for social media",
+      type: "string",
+    },
+    {
+      name: "url",
+      title: "Url",
+      type: "url",
+    },
+  ],
+};
