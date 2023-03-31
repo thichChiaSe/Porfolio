11585cd4f79b46d0589eea94b8cbd81cd5e5323e
Huydt13 <113592709+Huydt13@users.noreply.github.com>
2023-03-31 10:42:43 +0700

    feat : update skill

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,29 +1,21 @@
 import React from "react";
 import { motion } from "framer-motion";
-import js from "../public/java-script.png";
-import react from "../public/React-icon.svg";
-import nextjs from "../public/nextjs.png";
-import html from "../public/html.png";
-import css from "../public/css-3.png";
-import sass from "../public/sass.png";
-import typescript from "../public/typescript.png";
-import vercel from "../public/vercel.svg";
-type Props = { directionLeft?: boolean };
+type Props = { directionLeft?: boolean; skill: string; src: any; style: any };
 
-export default function Skill({ directionLeft }: Props) {
+export default function Skill({ directionLeft, skill, src, style }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
       <motion.img
         initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
-        src={html.src}
+        src={src}
         alt="icon-tech"
         className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
       />
-      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-gray h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
+      <div className="  absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-gray h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
-          <p className="text-3xl font-bold text-[#F7AB0B] opacity-100">HTML</p>
+          <p className={style}>{skill}</p>
         </div>
       </div>
     </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,6 +1,23 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Skill from "./Skill";
+import js from "../public/js.png";
+import react from "../public/React-icon.svg";
+import nextjs from "../public/nextjs.png";
+import html from "../public/html1.png";
+import css from "../public/css2.png";
+import sass from "../public/sass.png";
+import typescript from "../public/typescript.png";
+import redux from "../public/redux.png";
+import figma from "../public/figma.png";
+import tailwind from "../public/Tailwind_CSS_Logo.png";
+import saga from "../public/redux-saga.png";
+import tsc from "../public/styledcomponent.png";
+import pts from "../public/pts.png";
+import mui from "../public/mui.png";
+import ant from "../public/ant-design.jpg";
+import git from "../public/git.png";
+import dev from "../public/devextreme.png";
 type Props = {};
 
 export default function Skills({}: Props) {
@@ -9,16 +26,127 @@ export default function Skills({}: Props) {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         skills
       </h3>
-      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm mb-10">
         Hover over a skill for currency profieciency
       </h3>
-      <div className="grid grid-cols-4 gap-5">
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+      <div className="grid grid-cols-8 gap-5">
+        <Skill
+          skill={"HTML"}
+          src={html.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"CSS"}
+          src={css.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"SASS"}
+          src={sass.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"Javascript"}
+          src={js.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"ReactJS"}
+          src={react.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"Typescript"}
+          src={typescript.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"Nextjs"}
+          src={nextjs.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"Redux"}
+          src={redux.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"Redux saga"}
+          src={saga.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"Git"}
+          src={git.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"Tailwind"}
+          src={tailwind.src}
+          style={
+            ""
+          }
+        />
+        <Skill
+          skill={"Styled-component"}
+          src={tsc.src}
+          style={"text-1xl font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "}
+        />
+        <Skill
+          skill={"Photoshop"}
+          src={pts.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"Material UI"}
+          src={mui.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"Ant design"}
+          src={ant.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"Devextreme"}
+          src={dev.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
+        <Skill
+          skill={"Figma"}
+          src={figma.src}
+          style={
+            "font-bold text-[#F7AB0B] opacity-100 object-contain flex justify-center "
+          }
+        />
       </div>
     </motion.div>
   );
